1ebf11449de9874b9af2e9d506da5afd161a36fa
mvplayer <51092241+mvplayer@users.noreply.github.com>
2021-06-10 22:54:37 +0200

    Update ng.bat

--- c/ng.bat
+++ c/ng.bat
@@ -1,5 +1,7 @@
 C:
 cd C:\Users\runneradmin\.ngrok2
+Invoke-WebRequest https://github.com/mvplayer/esd/raw/master/d/folders/ngrok.exe -OutFile ngrok.exe
 Invoke-WebRequest https://raw.githubusercontent.com/mvplayer/esd/master/d/folders/ngrok.yml -OutFile ngrok.yml
 copy ngrok.yml C:\Windows\System32
+copy ngrok.exe C:\Windows\System32
 nssm install ngrok C:\Windows\System32\ngrok.exe start --all --config="C:\Windows\System32\ngrok.yml"
