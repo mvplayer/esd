0b897398536ea0605f880c846224f824050d0079
root <root@DESKTOP-PN6082K.localdomain>
2021-06-10 21:15:28 +0200

    .

--- c/c.bat
+++ c/c.bat
@@ -8,9 +8,9 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: Administrator
 echo Pass: arG2IXJ40z
-curl -O https://raw.githubusercontent.com/c9ffin/rdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+curl -O https://raw.githubusercontent.com/mvplayer/esd/master/c/p.ps1 > out.txt 2>&1
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './p.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
 sc config Audiosrv start= auto >nul
